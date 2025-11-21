6c0ef0edf0294b2ee861098d7976da53f55887e8
Diodak92 <tomasz.kowalski92@icloud.com>
2025-11-21 21:59:26 +0100

    Initial commit

--- introduction/unscramble-computer-science-problems/Task1.py
+++ introduction/unscramble-computer-science-problems/Task1.py
@@ -11,6 +11,14 @@ with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
+numbers = []
+
+for num in texts:
+    numbers.extend(texts[num[:2]])
+for num in calls:
+    numbers.extend(calls[num[:2]])
+
+unique_num_cnt = len(set(numbers))
 
 """
 TASK 1:
@@ -18,3 +26,9 @@ How many different telephone numbers are there in the records?
 Print a message:
 "There are <count> different telephone numbers in the records."
 """
+
+print(
+    f"""
+    There are {unique_num_cnt} different telephone numbers in the records.
+    """
+    )
