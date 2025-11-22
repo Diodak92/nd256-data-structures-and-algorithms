08d924ecceb91128a115974db6d01cc465a2c0a0
Diodak92 <tomasz.kowalski92@icloud.com>
2025-11-22 11:21:33 +0100

    Code cleanup

--- introduction/unscramble-computer-science-problems/Task2.py
+++ introduction/unscramble-computer-science-problems/Task2.py
@@ -11,6 +11,15 @@ with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
+"""
+TASK 2: Which telephone number spent the longest time on the phone
+during the period? Don't forget that time spent answering a call is
+also time spent on the phone.
+Print a message:
+"<telephone number> spent the longest time, <total time> seconds, on the phone during 
+September 2016.".
+"""
+
 max_call_length = None
 ph_num = None
 
@@ -23,13 +32,3 @@ for data in calls:
         ph_num = data[1]
 
 print(f"{ph_num} spent the longest time, {max_call_length} seconds, on the phone during September 2016")
-
-"""
-TASK 2: Which telephone number spent the longest time on the phone
-during the period? Don't forget that time spent answering a call is
-also time spent on the phone.
-Print a message:
-"<telephone number> spent the longest time, <total time> seconds, on the phone during 
-September 2016.".
-"""
-
