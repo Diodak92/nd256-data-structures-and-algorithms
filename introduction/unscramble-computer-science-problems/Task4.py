7b43874e165f434bf09f6bb11caab60444b07e24
Diodak92 <tomasz.kowalski92@icloud.com>
2025-11-22 11:21:22 +0100

    Completed task 4

--- introduction/unscramble-computer-science-problems/Task4.py
+++ introduction/unscramble-computer-science-problems/Task4.py
@@ -25,3 +25,29 @@ Print a message:
 The list of numbers should be print out one per line in lexicographic order with no duplicates.
 """
 
+# Telemarketers
+text_senders = []
+text_receivers = []
+phone_callers = []
+phone_receivers = []
+telemarketers = []
+
+for data in texts:
+    if data[0] not in text_senders:
+        text_senders.append(data[0])
+    if data[1] not in text_receivers:
+        text_receivers.append(data[1])
+
+for data in calls:
+    if data[0] not in phone_callers:
+        phone_callers.append(data[0])
+    if data[1] not in phone_receivers:
+        phone_receivers.append(data[1])
+
+for caller in phone_callers:
+    if (caller not in text_senders) and (caller not in text_receivers) and (caller not in phone_receivers):
+        telemarketers.append(caller)
+
+print("These numbers could be telemarketers: ")
+for ph_num in sorted(telemarketers):
+    print(ph_num)
